fc4e0e47ab4e78f432aff22ab89de2e0cb593bd4
Nic Haydel <nhaydel@cdisc.org>
2022-10-13 15:28:38 +0000

    Merged PR 1061: Add image upload functionality
    Add image upload functionality

--- utilities/wiki_document_parser.py
+++ utilities/wiki_document_parser.py
@@ -4,14 +4,16 @@ from markdown import markdown
 from db_models.ig_document import IGDocument
 from uuid import uuid4
 from utilities.transformer import Transformer
-
 from utilities.wiki_client import WikiClient
 from logging import Logger, getLogger
+from utilities.blob_service import BlobService
+import os
 
 class Parser:
     
     def __init__(self, client, logger=None):
         self.client: WikiClient = client
+        self.image_blob_service: BlobService = BlobService("images")
         self.logger: Logger = logger or getLogger("wiki-parser")
         self.transformer = Transformer()
     
@@ -37,7 +39,7 @@ class Parser:
             children = self._get_page_children(page_data["id"])
             html = page_data["body"]["view"]["value"]
             markdown_data = self._html_to_markdown(html)
-            parsed_html = self._parse_html(html)
+            parsed_html = self._parse_html(html, page_data["id"])
             labels = self._get_labels(page_data["id"])
             sections = [" ".join(section.split("-")[1:]) for section in labels if section.startswith("section-")]
             domains = [domain.split("-")[1].upper() for domain in labels if domain.startswith("domain-")]
@@ -81,17 +83,37 @@ class Parser:
 
     def _html_to_markdown(self, html_data):
         return markdownify(str(html_data), strip=['a'])
-    
-    def _parse_html(self, html) -> str:
+
+    def _parse_html(self, html, page_id) -> str:
         parser = BeautifulSoup(html, 'html.parser')
         for span in parser.find_all("span", {'class': 'jira-issue'}):
             # Remove jira issues in content
             span.decompose()
         for div in parser.find_all("div", {'class':'confluence-information-macro'}):
             div.decompose()
+        for div in parser.find_all("div", {'class':'plugin-tabmeta-details'}):
+            div.decompose()
         for div in parser.find_all("div", {'class':'expand-control'}):
             # Remove table dropdowns
             div.decompose()
+        for img in parser.find_all("img"):
+            img_link = img.attrs["src"]
+            try:
+                data = self.client.download_file(img_link)
+                image_path = img_link.split("?")[0]
+                image_file_name = f"{page_id}-{image_path.split('/')[-1]}"
+                self.image_blob_service.upload_file(data, image_file_name)
+                base_url = f"https://cdisclibrary{os.environ.get('ENVIRONMENT', '').lower()}.blob.core.windows.net/images"
+                image_link_path = f"{base_url}/{image_file_name}"
+                attrs = {
+                    "width": img.attrs.get("width", 500),
+                    "height": img.attrs.get("height", 500),
+                    "src": image_link_path
+                }
+                img.attrs = attrs
+                print(f"Successfully duplicated {img_link}")
+            except:
+                print(f"Failed to duplicate {img_link}")
         if parser.a:
             parser.a.extract()
         return self.transformer.get_raw_text(str(parser))
